02c6e62a52ef70cccae92630ef6e3cfdf76d784a
xcelxlorx <xcelxlorx@gmail.com>
2023-04-26 23:49:53 +0900

    Initial commit

---
+++ src/main/java/com/awsbasic/awsbasicservice/AwsBasicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.awsbasic.awsbasicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBasicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBasicServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awsbasic/awsbasicservice/AwsBasicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsbasic.awsbasicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBasicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
